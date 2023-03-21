5b5502f3bbd5e033d64c30f92fe61f71ad7d88c6
Stephen Roy <kstephenroy@deloitte.com>
2023-03-21 13:04:47 +0530

    Removed unused imports

--- src/main/java/com/aphc/displayboard/DisplayBoardApplication.java
+++ src/main/java/com/aphc/displayboard/DisplayBoardApplication.java
@@ -1,9 +1,7 @@
 package com.aphc.displayboard;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
